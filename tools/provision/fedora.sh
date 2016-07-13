ab5d2fed9b9b3d0702d27b1beca7c9d6fa75bec8
Teddy Reed <teddy@prosauce.org>
2016-07-13 15:25:00 -0700

    Clean up aux OSes

--- tools/provision/fedora.sh
+++ tools/provision/fedora.sh
@@ -8,73 +8,15 @@
 #  of patent rights can be found in the PATENTS file in the same directory.
 
 function distro_main() {
-  sudo dnf update -y
+  do_sudo dnf update -y
 
-  package texinfo
   package wget
-  package git-all
+  package git
   package unzip
+  package gawk
   package xz
-  package xz-devel
-  package python-pip
-  package python-devel
-  package rpm-build
   package ruby
-  package ruby-devel
-  package rubygems
-  package bzip2
-  package bzip2-devel
-  package openssl-devel
-  package readline-devel
-  package rpm-devel
-  package libblkid-devel
+  package ruby-irb
   package gcc
-  package binutils
-  package gcc-c++
-  package clang
-  package clang-devel
-
-  set_cc clang
-  set_cxx clang++
-
-  if [[ $DISTRO -lt "22" ]]; then
-    install_cmake
-  else
-    package cmake
-  fi
-
-  package doxygen
-  package byacc
-  package flex
-  package bison
-  package autoconf
-  package automake
-  package libtool
-
-  install_boost
-  install_gflags
-  install_glog
-  install_google_benchmark
-
-  install_snappy
-  install_rocksdb
-  install_thrift
-  install_yara
-  install_asio
-  install_cppnetlib
-  install_sleuthkit
-
-  # Device mapper uses the exact version as the ABI.
-  # We will build and install a static version.
-  remove_package device-mapper-devel
-  package libgcrypt-devel
-  package gettext-devel
-
-  install_device_mapper
-  install_iptables_dev
-  install_libcryptsetup
-
-  gem_install fpm
-
-  install_aws_sdk
+  package bzip2
 }
