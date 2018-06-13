b193085e8ca60cd3445006a31b2c4ff6276de7a9
Teddy Reed <teddy@prosauce.org>
2018-06-13 18:33:14 -0400

    deps: Update libgcrypt to version 1.8.3

--- tools/provision/formula/libgcrypt.rb
+++ tools/provision/formula/libgcrypt.rb
@@ -4,15 +4,15 @@ class Libgcrypt < AbstractOsqueryFormula
   desc "Cryptographic library based on the code from GnuPG"
   homepage "https://directory.fsf.org/wiki/Libgcrypt"
   license "LGPL-2.1+"
-  url "https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.1.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.8.1.tar.bz2"
-  sha256 "7a2875f8b1ae0301732e878c0cca2c9664ff09ef71408f085c50e332656a78b3"
+  url "https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.3.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.8.3.tar.bz2"
+  sha256 "66ec90be036747602f2b48f98312361a9180c97c68a690a5f376fa0f67d0af7c"
   revision 200
 
   bottle do
     root_url "https://osquery-packages.s3.amazonaws.com/bottles"
     cellar :any_skip_relocation
-    sha256 "b8a827a90f1514b518362094f8ab39e557d96690306c9bef131ffe34eeac16b6" => :x86_64_linux
+    sha256 "8935958d65b3912c3240361455a02353ac3bea6f090bcb27307367d85299e606" => :x86_64_linux
   end
 
   depends_on "libgpg-error"
@@ -29,21 +29,16 @@ class Libgcrypt < AbstractOsqueryFormula
     ENV.universal_binary if build.universal?
 
     args = [
-      "--disable-dependency-tracking",
-      "--disable-silent-rules",
       "--disable-avx-support",
       "--disable-avx2-support",
       "--disable-drng-support",
       "--disable-pclmul-support",
-      "--disable-shared",
-      "--enable-static",
-      "--prefix=#{prefix}",
       "--disable-asm",
-      "--with-libgpg-error-prefix=#{Formula["libgpg-error"].opt_prefix}",
-      "--with-gpg-error-prefix=#{Formula["libgpg-error"].opt_prefix}",
+      "--with-libgpg-error-prefix=#{Formula["osquery/osquery-local/libgpg-error"].opt_prefix}",
+      "--with-gpg-error-prefix=#{Formula["osquery/osquery-local/libgpg-error"].opt_prefix}",
     ]
 
-    system "./configure", *args
+    system "./configure", *osquery_autoconf_flags, *args
     if build.universal?
       buildpath.install resource("config.h.ed")
       system "ed -s - config.h <config.h.ed"
