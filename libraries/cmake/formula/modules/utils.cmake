6a4ae50ea68b3794675f94c83839f0483f372224
Alessandro Gario <alessandro.gario@gmail.com>
2019-08-24 13:14:21 +0200

    CMake: Fix formula errors breaking auto re-configuration

--- libraries/cmake/formula/modules/utils.cmake
+++ libraries/cmake/formula/modules/utils.cmake
@@ -16,55 +16,35 @@ function(importFormula library_name)
 
   message(STATUS "Importing formula: formula/${library_name}")
 
-  # Make sure the source folder exists
-  set(project_directory_path "${CMAKE_SOURCE_DIR}/libraries/cmake/formula/${library_name}")
+  if(NOT ${library_name}_formula_initialized)
+    # Make sure the source folder exists
+    set(project_directory_path "${CMAKE_SOURCE_DIR}/libraries/cmake/formula/${library_name}")
 
-  if(NOT EXISTS "${project_directory_path}")
-    message(FATAL_ERROR "The formula was not found: ${project_directory_path}")
-  endif()
+    if(NOT EXISTS "${project_directory_path}")
+      message(FATAL_ERROR "The formula was not found: ${project_directory_path}")
+    endif()
 
-  # Create the build folder in advance
-  set(build_directory_path "${CMAKE_CURRENT_BINARY_DIR}/${library_name}")
+    # Create the build folder in advance
+    set(build_directory_path "${CMAKE_CURRENT_BINARY_DIR}/${library_name}")
 
-  execute_process(
-    COMMAND "${CMAKE_COMMAND}" -E make_directory "${build_directory_path}"
-    RESULT_VARIABLE error
-  )
-
-  if(NOT ${error} EQUAL 0)
-    message(FATAL_ERROR "The formula build folder could not be created: ${build_directory_path}")
-  endif()
-
-  # We need to configure the project to capture its metadata
-  set(install_prefix "${OSQUERY_FORMULA_INSTALL_DIRECTORY}/${library_name}")
-
-  if(DEFINED PLATFORM_WINDOWS)
-    set(toolset_option -T "${CMAKE_GENERATOR_TOOLSET}")
-  endif()
-
-  execute_process(
-    COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" ${toolset_option} "-DCMAKE_C_COMPILER:STRING=${CMAKE_C_COMPILER}" "-DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}" "-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}" "-DCMAKE_INSTALL_PREFIX:STRING=${install_prefix}" "${project_directory_path}"
-    WORKING_DIRECTORY "${build_directory_path}"
-    RESULT_VARIABLE error
-    OUTPUT_VARIABLE std_output
-    ERROR_VARIABLE std_error
-  )
-
-  if(NOT ${error} EQUAL 0)
-    message(FATAL_ERROR "The ${library_name} formula could not be configured\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
-  endif()
-
-  # Get the formula metadata
-  set(metadata_field_list
-    metadata_version
-    metadata_revision
-    metadata_dependencies
-    metadata_libraries
-  )
-
-  foreach(metadata_field ${metadata_field_list})
     execute_process(
-      COMMAND "${CMAKE_COMMAND}" --build . --config "${CMAKE_BUILD_TYPE}" --target "${metadata_field}"
+      COMMAND "${CMAKE_COMMAND}" -E make_directory "${build_directory_path}"
+      RESULT_VARIABLE error
+    )
+
+    if(NOT ${error} EQUAL 0)
+      message(FATAL_ERROR "The formula build folder could not be created: ${build_directory_path}")
+    endif()
+
+    # We need to configure the project to capture its metadata
+    set(install_prefix "${OSQUERY_FORMULA_INSTALL_DIRECTORY}/${library_name}")
+
+    if(DEFINED PLATFORM_WINDOWS)
+      set(toolset_option -T "${CMAKE_GENERATOR_TOOLSET}")
+    endif()
+
+    execute_process(
+      COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" ${toolset_option} "-DCMAKE_C_COMPILER:STRING=${CMAKE_C_COMPILER}" "-DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}" "-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}" "-DCMAKE_INSTALL_PREFIX:STRING=${install_prefix}" "${project_directory_path}"
       WORKING_DIRECTORY "${build_directory_path}"
       RESULT_VARIABLE error
       OUTPUT_VARIABLE std_output
@@ -72,40 +52,60 @@ function(importFormula library_name)
     )
 
     if(NOT ${error} EQUAL 0)
-      message(FATAL_ERROR "Failed to acquire the metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
+      message(FATAL_ERROR "The ${library_name} formula could not be configured\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
     endif()
 
-    string(FIND "${std_output}" "[" value_start_index)
-    if(${value_start_index} EQUAL -1)
-      message(FATAL_ERROR "Malformed metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
-    endif()
+    # Get the formula metadata
+    set(metadata_field_list
+      metadata_version
+      metadata_revision
+      metadata_dependencies
+      metadata_libraries
+    )
 
-    string(FIND "${std_output}" "]" value_end_index)
-    if(${value_end_index} EQUAL -1)
-      message(FATAL_ERROR "Malformed metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
-    endif()
+    foreach(metadata_field ${metadata_field_list})
+      execute_process(
+        COMMAND "${CMAKE_COMMAND}" --build . --config "${CMAKE_BUILD_TYPE}" --target "${metadata_field}"
+        WORKING_DIRECTORY "${build_directory_path}"
+        RESULT_VARIABLE error
+        OUTPUT_VARIABLE std_output
+        ERROR_VARIABLE std_error
+      )
 
-    math(EXPR value_start_index "${value_start_index} + 1")
-    math(EXPR value_char_length "${value_end_index} - ${value_start_index}")
+      if(NOT ${error} EQUAL 0)
+        message(FATAL_ERROR "Failed to acquire the metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
+      endif()
 
-    if(${value_char_length} EQUAL 0)
-      if("${metadata_field}" STREQUAL "metadata_dependencies")
-        continue()
-      else()
+      string(FIND "${std_output}" "[" value_start_index)
+      if(${value_start_index} EQUAL -1)
         message(FATAL_ERROR "Malformed metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
       endif()
-    endif()
 
-    string(SUBSTRING "${std_output}" ${value_start_index} ${value_char_length} metadata_field_value)
-    if("${metadata_field_value}" STREQUAL "")
-      message(FATAL_ERROR "Malformed metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
-    endif()
+      string(FIND "${std_output}" "]" value_end_index)
+      if(${value_end_index} EQUAL -1)
+        message(FATAL_ERROR "Malformed metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
+      endif()
 
-    set("${metadata_field}" "${metadata_field_value}")
-  endforeach()
+      math(EXPR value_start_index "${value_start_index} + 1")
+      math(EXPR value_char_length "${value_end_index} - ${value_start_index}")
 
-  # Generate the options necessary to pass the dependencies to the formula
-  if(DEFINED metadata_dependencies)
+      if(${value_char_length} EQUAL 0)
+        if("${metadata_field}" STREQUAL "metadata_dependencies")
+          continue()
+        else()
+          message(FATAL_ERROR "Malformed metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
+        endif()
+      endif()
+
+      string(SUBSTRING "${std_output}" ${value_start_index} ${value_char_length} metadata_field_value)
+      if("${metadata_field_value}" STREQUAL "")
+        message(FATAL_ERROR "Malformed metadata field ${metadata_field} for formula ${library_name}\nstdout\n===\n${std_output}\n\n\nstderr\n===\n${std_error}")
+      endif()
+
+      set("${metadata_field}" "${metadata_field_value}")
+    endforeach()
+
+    # Generate the options necessary to pass the dependencies to the formula
     foreach(dependency ${metadata_dependencies})
       if(NOT TARGET "${dependency}")
         message(FATAL_ERROR "Invalid dependency specified: ${dependency}")
@@ -121,13 +121,22 @@ function(importFormula library_name)
         "-D${dependency}_LIBRARIES:STRING=$<TARGET_FILE:${dependency}>"
       )
     endforeach()
-  endif()
 
-  # Generate the formula runner that will actually build the libraries
-  # TODO(alessandro): Change it so we use a completely new environment
-  foreach(output_lib ${metadata_libraries})
-    list(APPEND output_file_list "${install_prefix}/${output_lib}")
-  endforeach()
+    # Generate the formula runner that will actually build the libraries
+    # TODO(alessandro): Change it so we use a completely new environment
+    foreach(output_lib ${metadata_libraries})
+      list(APPEND output_file_list "${install_prefix}/${output_lib}")
+    endforeach()
+
+    set(${library_name}_output_file_list "${output_file_list}" CACHE STRING "${library_name} output files")
+    set(${library_name}_formula_dependency_settings "${formula_dependency_settings}" CACHE STRING "${library_name} dependency settings")
+    set(${library_name}_metadata_dependencies "${metadata_dependencies}" CACHE STRING "${library_name} dependencies")
+
+    set(${library_name}_metadata_version "${metadata_version}" CACHE STRING "${library_name} version")
+    set(${library_name}_metadata_revision "${metadata_revision}" CACHE STRING "${library_name} revision")
+    
+    set(${library_name}_formula_initialized true CACHE BOOL "${library_name} initialized")
+  endif()
 
   set(log_folder_path
     "${CMAKE_BINARY_DIR}/formula_logs"
@@ -141,10 +150,10 @@ function(importFormula library_name)
   getCompilationFlags(cxx cxx_compilation_flags)
 
   add_custom_command(
-    OUTPUT ${output_file_list}
+    OUTPUT ${${library_name}_output_file_list}
     COMMAND "${CMAKE_COMMAND}" -E make_directory "${log_folder_path}"
     COMMAND "${CMAKE_COMMAND}" -E remove -f "${log_file_path}"
-    COMMAND "${CMAKE_COMMAND}" -DC_FLAGS:STRING="${c_compilation_flags}" -DCXX_FLAGS:STRING="${c_compilation_flags}" ${formula_dependency_settings} "${project_directory_path}" >> "${log_file_path}" 2>&1
+    COMMAND "${CMAKE_COMMAND}" -DC_FLAGS:STRING="${c_compilation_flags}" -DCXX_FLAGS:STRING="${c_compilation_flags}" ${${library_name}_formula_dependency_settings} "${project_directory_path}" >> "${log_file_path}" 2>&1
     COMMAND "${CMAKE_COMMAND}" --build . --config "${CMAKE_BUILD_TYPE}" >> "${log_file_path}" 2>&1
     WORKING_DIRECTORY "${build_directory_path}"
     COMMENT "Running formula: ${library_name} (${log_file_path})"
@@ -152,11 +161,11 @@ function(importFormula library_name)
   )
 
   add_custom_target("${library_name}_formula_runner"
-    DEPENDS ${output_file_list}
+    DEPENDS ${${library_name}_output_file_list}
   )
 
   add_dependencies("${library_name}_formula_runner"
-    ${metadata_dependencies}
+    ${${library_name}_metadata_dependencies}
   )
 
   # Generate the imported library
@@ -179,9 +188,9 @@ function(importFormula library_name)
     "${library_name}_formula_runner"
   )
 
-  message(STATUS "  Version: ${metadata_version}")
-  message(STATUS "  Revision: ${metadata_revision}")
-  message(STATUS "  Dependencies: ${metadata_dependencies}")
+  message(STATUS "  Version: ${${library_name}_metadata_version}")
+  message(STATUS "  Revision: ${${library_name}_metadata_revision}")
+  message(STATUS "  Dependencies: ${${library_name}_metadata_dependencies}")
 endfunction()
 
 function(getCompilationFlags language output_variable)
